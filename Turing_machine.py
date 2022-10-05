8edae25b9644b1937cb2b7d4e975e7f1f1588342
tmmarquess <thiago.uchoa18@gmail.com>
2022-10-04 22:37:32 -0400

    refatorando PEP8

--- Turing_machine.py
+++ Turing_machine.py
@@ -2,48 +2,47 @@ import json
 
 fita = str()
 
-movimento = {
-    'D': 1,
-    'E': -1,
-    'P': 0
-}
+movimento = {"D": 1, "E": -1, "P": 0}
+
 
 def reiniciar_fita(tam=100):
     global fita
-    fita = 'b'*tam
+    fita = "b" * tam
     fita = list(fita)
 
-def converter_para_dict(str : str) -> dict:
+
+def converter_para_dict(str: str) -> dict:
     str = str.replace("'", '"')
-    str = str.replace('(',('[')).replace(')',(']'))
-    str = str.replace('D',('"D"')).replace('E',('"E"'))
-    str = str.replace('P',('"P"')).replace('b',('"b"'))
+    str = str.replace("(", ("[")).replace(")", ("]"))
+    # str = str.replace('D',('"D"')).replace('E',('"E"'))
+    # str = str.replace('P',('"P"')).replace('b',('"b"'))
 
     try:
         return json.loads(str)
     except:
         return None
 
-def computar(palavra : list, mt : dict) -> bool:
+
+def computar(palavra: list, mt: dict) -> bool:
     global fita
     reiniciar_fita()
     for i in range(len(palavra)):
         fita[i] = palavra[i]
-    
+
     cabecote = 0
-    estado_atual = mt['inicial']
-    estados = mt['delta']
+    estado_atual = mt["inicial"]
+    estados = mt["delta"]
 
     while True:
         refazer_loop = False
         for estado in estados:
             if estado[0] == estado_atual and str(estado[2]) == fita[cabecote]:
-                    refazer_loop = True
-                    fita[cabecote] = str(estado[3])
-                    cabecote += movimento[estado[4]]
-                    estado_atual = estado[1]
+                refazer_loop = True
+                fita[cabecote] = str(estado[3])
+                cabecote += movimento[estado[4]]
+                estado_atual = estado[1]
         if not refazer_loop:
-            if estado_atual == mt['aceita']:
+            if estado_atual == mt["aceita"]:
                 return "ACEITA"
             else:
                 return "REJEITA"
@@ -64,9 +63,8 @@ for palavra in palavras:
 
     palavra_modificada = []
     i = 0
-    while(fita[i] != 'b'):
-        print(f'{fita[i]}',end='')
+    while fita[i] != "b":
+        print(f"{fita[i]}", end="")
         i += 1
 
-    print(f' {res}')
-    
+    print(f" {res}")
